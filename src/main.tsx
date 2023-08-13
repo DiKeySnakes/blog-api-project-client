2c5836ab1678ee7d6f11a11fb4bd66752a6a28c9
DiKeySnakes <dikeysnakes@gmail.com>
2023-08-13 09:26:41 +0300

    Add @fvilers/disable-react-devtools package, modify package-lock.json, package.json, src/app/store.ts, src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react"
+import { disableReactDevTools } from "@fvilers/disable-react-devtools"
 import App from "./App"
 import "./index.css"
+
+if (process.env.NODE_ENV === "production") disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
